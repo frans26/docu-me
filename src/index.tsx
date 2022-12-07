8654a86941ce91f58d17812bc85d295749a267db
Francis M <>
2022-12-07 16:35:10 +0800

    task: initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import reportWebVitals from "./reportWebVitals";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hi there!</h1>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
